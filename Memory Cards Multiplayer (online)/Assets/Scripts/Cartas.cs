28ec01a1f732b72910840c3e90218a7db461792a
nombre-original <diana.ortola@mail.ucv.es>
2025-11-28 17:03:37 +0100

    cartes es repartixen, cronòmetre fet

--- Memory Cards Multiplayer (online)/Assets/Scripts/Cartas.cs
+++ Memory Cards Multiplayer (online)/Assets/Scripts/Cartas.cs
@@ -1,17 +1,42 @@
 using UnityEngine;
 
 public class Cartas : MonoBehaviour{
+    public GameObject PRUEBA;
     public GameObject[] cartas;
     private int[] baraja = new int[7];
 
     private int numAleatorio;
     private int numCartas;
 
+    private float x;
+    private float y;
+    private float sepX;
+    private float sepY;
+
+    private bool cartasSobreLaMesa = false;
+
     void Start(){
+        x = -7.5f;
+        y = 2f;
+        sepX = 5f;
+        sepY = 4f;
         numCartas = cartas.Length;
     }
 //NO, CREAR UNA BARSE PREESTABLECIDA Y DESORDENARLA
     void Update(){
+        if (!cartasSobreLaMesa){
+            for (int i=0; i<=7; i++){ //Primeras 4 cartas
+                Instantiate(PRUEBA, new Vector2(x, y), Quaternion.identity/*para que ignore rotación*/);
+                x += sepX;
+                if (x>8f){ //Últimas 4 cartas
+                    y -= sepY;
+                    x = x-(sepX*4);
+                }
+            }
+            cartasSobreLaMesa = true;
+        }
+        
+        /*
         //ELEGIR CARTAS DE LA BARAJA
         for (int i=0; i<=cartas.Length; i++){
             numAleatorio = Random.Range(0,numCartas);//num aleatorio
@@ -33,7 +58,7 @@ public class Cartas : MonoBehaviour{
         }
         for (int i=0; i<=4; i++){
             Debug.Log(baraja[i]);
-        }*/
         }
+        }*/
     }
 }
