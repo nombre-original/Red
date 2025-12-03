242709870c303c4c5bf97f2061a14bca7986d6bc
nombre-original <diana.ortola@mail.ucv.es>
2025-12-03 19:14:42 +0100

    no mucho ha cambiado

--- Memory Cards Multiplayer (online)/Assets/Scripts/Cartas.cs
+++ Memory Cards Multiplayer (online)/Assets/Scripts/Cartas.cs
@@ -5,6 +5,7 @@ public class Cartas : MonoBehaviour{
     public Sprite[] cartaTipo;
     public GameObject carta;
     private int[] baraja = new int[7];
+    private int eleccion;
 
     private int numAleatorio;
 
@@ -25,10 +26,15 @@ public class Cartas : MonoBehaviour{
 //NO, CREAR UNA BARSE PREESTABLECIDA Y DESORDENARLA
     void Update(){
         if (!cartasSobreLaMesa){
+            //tipos de cartas
+            for (int a = 0; a<=3; a++){
+                eleccion = Random.Range(1,cartaTipo.Length-1);
+                baraja[a] = eleccion;
+            }
+            //cartas
             for (int i=0; i<=7; i++){ //Primeras 4 cartas
-                numAleatorio = Random.Range(1,cartaTipo.Length-1);//num aleatorio
-                //Instantiate(cartas[numAleatorio], new Vector2(x, y), Quaternion.identity/*ignora apartado rotación*/);
                 Instantiate(carta, new Vector2(x, y), Quaternion.identity);
+                //baraja[i] = carta.GetComponent<VoltearCarta>().tipo;
                 Debug.Log("detrás");
                 x += sepX;
                 if (x>8f){ //Últimas 4 cartas
@@ -36,32 +42,8 @@ public class Cartas : MonoBehaviour{
                     x = x-(sepX*4);
                 }
             }
+            Debug.Log(baraja[3]);
             cartasSobreLaMesa = true;
         }
-        
-        /*
-        //ELEGIR CARTAS DE LA BARAJA
-        for (int i=0; i<=cartas.Length; i++){
-            numAleatorio = Random.Range(0,numCartas);//num aleatorio
-            baraja[i] = numAleatorio;
-            Debug.Log(baraja[i]);
-            //REPETICIONES
-            /*for (int a=0; a<=baraja[i-1]; a++){//si se repite 1 vez
-                if(numAleatorio == baraja[a]){
-                    for (int b=0; b<=baraja[a+1]; b++){//si se repite dos veces
-                        if(numAleatorio == baraja[b]){
-                            numAleatorio = Random.Range(0,cartas.Length);
-                        }
-                    }
-                //SI NO SE REPITE
-                } else {
-                    baraja[i] = numAleatorio;
-                }
-            }
-        }
-        for (int i=0; i<=4; i++){
-            Debug.Log(baraja[i]);
-        }
-        }*/
     }
 }
--- Memory Cards Multiplayer (online)/Assets/Scripts/VoltearCarta.cs
+++ Memory Cards Multiplayer (online)/Assets/Scripts/VoltearCarta.cs
@@ -1,23 +1,21 @@
 using UnityEngine;
 
-public class CambioCartas : MonoBehaviour{
+public class VoltearCarta : MonoBehaviour{
     public SpriteRenderer spriteRenderer;
     public Sprite[] carta;
     public bool girada;
+    public int tipo;
 
     void Start(){
         girada = false;
+        tipo = Random.Range(1,carta.Length-1);
     }
     
     void Update(){
         if (!girada){
             spriteRenderer.sprite = carta[0];
         } else {
-            Cambio();
+            spriteRenderer.sprite = carta[tipo];
         }
-    }
-
-    public void Cambio(){
-        spriteRenderer.sprite = carta[1];
     }
 }
