4e0882ef488d751a4be81b8de824441aa6098f5f
nombre-original <diana.ortola@mail.ucv.es>
2025-12-19 16:57:34 +0100

    estaba cambiando el pinguconejo

--- Juego Red (Online)/Assets/Scripts/Juego/Rabbit/Rabbit.cs
+++ Juego Red (Online)/Assets/Scripts/Juego/Rabbit/Rabbit.cs
@@ -1,6 +1,10 @@
 using UnityEngine;
 
 public class Rabbit : MonoBehaviour{
+    //locations
+    private float x = 0;
+    private float y = 0;
+
     private int direction;
     private float distance;
     private float wait;
@@ -8,7 +12,7 @@ public class Rabbit : MonoBehaviour{
     private bool inside = false;
     private int speed = 3;
     void Start(){
-        
+
     }
 
     void Update(){
@@ -18,7 +22,13 @@ public class Rabbit : MonoBehaviour{
     }
 
     private void Run(){
-        
+        direction = Random.Range(0,4);
+        distance = Random.Range(0.5,3);
+
+        if (direction == 0) transform.Translate(Vector3.left * Time.deltaTime, Space.World);
+        if (direction == 1) transform.Translate(Vector3.right * Time.deltaTime, Space.World);
+        if (direction == 2) transform.Translate(Vector3.up * Time.deltaTime, Space.World);
+        if (direction == 3) transform.Translate(Vector3.down * Time.deltaTime, Space.World);
     }
     
     void Grabbed(){
